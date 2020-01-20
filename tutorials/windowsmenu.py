7a94542b19845c955ce31412f00a577a00cdceb3
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-01-20 18:02:44 +0300

    Listbox and Scrollbar

--- tutorials/windowsmenu.py
+++ tutorials/windowsmenu.py
@@ -1,8 +1,11 @@
 from tkinter import *
+import os
 
 root = Tk()
+root.geometry('640x480-8-200')
 menu = Menu(root)
 root.config(menu=menu)
+
 filemenu = Menu(menu) # creating a File menu
 menu.add_cascade(label="File", menu=filemenu)
 
@@ -19,6 +22,20 @@ helpmenu = Menu(menu) # creating a help menu
 menu.add_cascade(label="Help", menu=helpmenu)
 helpmenu.add_command(label="Troubleshoot")
 helpmenu.add_command(label="About")
+
+filelist = Listbox(root)
+filelist.grid(row=0, column=0)
+filelist.config(border=2, relief='sunken')
+
+
+scrollbar = Scrollbar(root, orient=VERTICAL, command=filelist.yview)
+scrollbar.grid(row=0, column=2, sticky="nsw", columnspan=2)
+filelist['yscrollcommand'] = scrollbar.set
+
+for zone in os.listdir('D:/postgresql/bin'):
+    filelist.insert(END, zone)
+
+
 
 
 mainloop()
